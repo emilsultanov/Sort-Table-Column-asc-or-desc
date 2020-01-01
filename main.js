87427a1f78f786566f9639f38e2e960d6474388a
Emil <emilsultanov01@gmail.com>
2020-01-01 22:27:42 +0400

    optimizing2

--- main.js
+++ main.js
@@ -126,19 +126,21 @@ const data = [
 
 let sliceData;
 let filterType = 'asc';
-let pagination__list = document.querySelector('.pagination__list');
 let rowsPerPage = 3;
+let pagination__list = document.querySelector('.pagination__list');
 let pagination_items_count = Math.ceil(data.length / rowsPerPage);
 
 
 // Create dnyamic pagination items
 for (let i = 1; i <= pagination_items_count; i++) {
+
    let pagination_item = document.createElement('li');
    let node = document.createTextNode(i);
    pagination_item.appendChild(node);
    pagination_item.classList.add('pagination__item');
    pagination_item.addEventListener('click', pagination__item_handler);
    pagination__list.appendChild(pagination_item);
+
 }
 pagination__list.children[0].classList.add('pagination__item_isActive');
 
@@ -146,30 +148,36 @@ pagination__list.children[0].classList.add('pagination__item_isActive');
 
 // Get index of clicked pagination item
 function pagination__item_handler(e) {
+
    let clickedIndex = Array.from(document.querySelectorAll('.pagination__item')).indexOf(this);
-   Array.from(pagination__list.children).forEach((child) => { child.classList.remove('pagination__item_isActive') });
+   Array.from(pagination__list.children).forEach(child => child.classList.remove('pagination__item_isActive'));
    this.classList.add('pagination__item_isActive');
    divideData(data, rowsPerPage, clickedIndex);
+
 }
 
 
 
 // Slice data for rows per page
 function divideData(items, rows_per_page, page) {
+
    let start = rows_per_page * page;
    let end = start + rows_per_page;
    let partOfData = data.slice(start, end);
    sliceData = partOfData;
    loadTableContent(partOfData);
+
 }
 divideData(data, rowsPerPage, 0);
 
 
 let pagination_btns = Array.from(document.querySelectorAll('[data-pagination-direct]'));
-pagination_btns.forEach((btn) => { btn.addEventListener('click', pagination_btn_handler); });
+pagination_btns.forEach(btn => btn.addEventListener('click', pagination_btn_handler));
 
 function pagination_btn_handler() {
+
    let paginationDirect = this.dataset.paginationDirect;
+
    if (paginationDirect === 'prev') {
       for (let i = 0; i < pagination__list.children.length; i++) {
          if (pagination__list.children[i].classList.contains('pagination__item_isActive') && pagination__list.children[i].previousElementSibling) {
@@ -181,6 +189,7 @@ function pagination_btn_handler() {
       }
    }
    else {
+
       for (let i = 0; i < pagination__list.children.length; i++) {
          if (pagination__list.children[i].classList.contains('pagination__item_isActive') && pagination__list.children[i].nextElementSibling) {
             pagination__list.children[i].classList.remove('pagination__item_isActive');
@@ -190,12 +199,14 @@ function pagination_btn_handler() {
          }
       }
    }
-}
+
+};
 
 
 // Update Table Content
 function loadTableContent(filteredData) {
-   let table = filteredData.map((item) => {
+
+   let table = filteredData.map(item => {
       return (
          `<tr>
             <td>${item.no}</td>
@@ -211,6 +222,7 @@ function loadTableContent(filteredData) {
       );
    });
    document.querySelector('.bodyOfSortableTable').innerHTML = table.join('');
+
 };
 
 
@@ -218,26 +230,28 @@ function loadTableContent(filteredData) {
 
 // Select Table Title and Add eventlistener
 let filterTitles = document.querySelectorAll('[data-filter-value]');
-filterTitles.forEach((title) => {
-   title.addEventListener('click', filterColumn);
-});
+filterTitles.forEach(title => title.addEventListener('click', filterColumn));
 
 
 function filterColumn(e) {
+
    sort__array(sliceData, e.target.dataset.filterValue, filterType);
    let lastActiveElements = document.querySelectorAll('th[data-filter-value]');
-   lastActiveElements.forEach((element) => {
-      element.classList.remove('activeTitle');
-   });
+   lastActiveElements.forEach(element => element.classList.remove('activeTitle'));
    e.target.classList.add('activeTitle');
+
 };
 
 
 // Sort Table Content
 function sort__array(arr, filter, type) {
+
    if (type == 'asc') {
+
       for (let i = 0; i < arr.length; i++) {
+
          for (let j = arr.length - 1; j > i; j--) {
+
             if (arr[i][filter] > arr[j][filter]) {
                let max = arr[i];
                arr[i] = arr[j];
@@ -249,8 +263,11 @@ function sort__array(arr, filter, type) {
       filterType = 'desc';
    }
    else {
+
       for (let i = 0; i < arr.length; i++) {
+
          for (let j = arr.length - 1; j > i; j--) {
+
             if (arr[i][filter] < arr[j][filter]) {
                let min = arr[j];
                arr[j] = arr[i];
